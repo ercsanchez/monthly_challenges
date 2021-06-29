5533e41d9c55d8d685e38805166dcd70fcc08471
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-06-29 21:35:48 +0000

    add urls and views for january and february routes in challenges app

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Index of the Challenges App!")
+
+def january(request):
+    return HttpResponse("Challenge for January")
+
+def february(request):
+    return HttpResponse("Challenge for February")
