0b30dcb2b2a7ce34415ff1cc2a4aa635cbc08053
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-06-29 22:35:41 +0000

    use url params to capture urls dynamically

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("january/", views.january),
-    path("february/", views.february)
+    # path("january/", views.january),
+    # path("february/", views.february),
+    path("<month>/", views.monthly_challenge)
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 def index(request):
     return HttpResponse("Index of the Challenges App!")
 
-def january(request):
-    return HttpResponse("Challenge for January")
+# def january(request):
+#     return HttpResponse("Challenge for January")
 
-def february(request):
-    return HttpResponse("Challenge for February")
+# def february(request):
+#     return HttpResponse("Challenge for February")
+
+def monthly_challenge(request, month):
+    if month == "january" or month == "february" or month == "march":
+        return HttpResponse(f"Challenge for {month}")
+    else:
+        return HttpResponseNotFound("This month is not supported")
