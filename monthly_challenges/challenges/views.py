ecee6d37b6e7f9bbd7c1b3237e6b61fadcf91ff1
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-06-30 01:20:51 +0000

    use redirect

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 monthly_challenges = {
     "january": "challenge for january",
@@ -29,7 +29,14 @@ def index(request):
 
 
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(f"This is a month represented by the number {month}")
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month!")
+
+    redirect_month = months[month-1]
+    # return HttpResponse(f"This is a month represented by the number {month}")
+    return HttpResponseRedirect(f"/challenges/{redirect_month}/")
 
 
 def monthly_challenge(request, month):
